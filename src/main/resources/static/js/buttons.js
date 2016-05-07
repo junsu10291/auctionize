4dfe21002a3bf1759f0d605f564c7329a94da7bd
Nate Brennan <nathaniel_brennan@brown.edu>
2016-05-07 15:59:48 -0400

    slider 5 min steps

--- src/main/resources/static/js/buttons.js
+++ src/main/resources/static/js/buttons.js
@@ -2,7 +2,7 @@ var categories = ["YARD","CLEAN","COURIER","PAINT","ACT","MOVE",
                   "CONSTRUCT","HANDY","DRIVE","PET","BABY","ASSEMBLE","MISC"];
 
 $( document ).ready(function() {
-    $("#slider").slider({ id: "slider-id", min: 480, max: 1440, range: true, value: [480, 1440] });
+    $("#slider").slider({ id: "slider-id", min: 96, max: 288, range: true, value: [96, 288] });
 });
 
 // trying to change the time displayed on the day of jobs button
@@ -12,7 +12,7 @@ $("#slider").on("slide", function(slideEvent) {
 });
 
 function sliderValToTime(sliderVal) {
-  var hour = Math.floor(sliderVal / 60);
-  var minute = sliderVal % 60;
+  var hour = Math.floor(sliderVal / 12);
+  var minute = (sliderVal % 12) * 5;
   return {hour: hour, minute: minute};
 }
